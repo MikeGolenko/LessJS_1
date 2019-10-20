273fbef57e76e9dcef133b2735c504d3245f878c
MikeGolenko <mihgeng@mail.ru>
2019-10-20 21:19:43 +0700

    new script

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let incr = 10,
-    decr = 10;
-incr++;
-decr--;
+let money = prompt("Ваш бюджет за месяц?", '');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-console.log(decr);
-console.log(incr);
+appData = {
+    budget : money,
+    timeData : time,
+    expeses : {},
+    optionalExpenses : {},
+    incom: [],
+    savings : false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expeses.a1 = a2;
+appData.expeses.a3 = a4;
+
+alert(appData.budget / 30);
